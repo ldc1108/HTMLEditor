51b02eac8275b9ceb651895fa51a4dbdc1565cff
unknown <ldc1108@DAVIES.se.rit.edu>
2013-03-22 18:49:21 -0400

    Our current project (the second email)
    This is with the one with the new tabs working.

---
+++ src/htmleditor/Buffer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htmleditor;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author Danielle Neuberger and Luke Coy
+ */
+public class Buffer extends Observable{
+    String contents = "";
+    int number;    
+    
+    public Buffer(int number, String contents){
+        this.number = number;
+        this.contents = contents;
+    }
+    
+}
---
+++ src/htmleditor/HTMLEditor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htmleditor;
+
+/**
+ *
+ * @author ldc1108
+ */
+public class HTMLEditor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello! Danielle!");
+        testGUI g = new testGUI();
+        String[] s = new String[0];
+        g.main(s);        
+    }
+}
