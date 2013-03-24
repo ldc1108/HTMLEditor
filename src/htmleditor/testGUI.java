7e8e95c728fde0453b1d6c5ecf5d361d91208a0d
Luke Coy <ldc1108@rit.edu>
2013-03-24 19:27:14 -0400

    Made system default the look and feel when running
    On windows, goes to windows.

--- src/htmleditor/testGUI.java
+++ src/htmleditor/testGUI.java
@@ -531,12 +531,7 @@ public class testGUI extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
+            javax.swing.UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(testGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
